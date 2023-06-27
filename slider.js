89f5e9bd46796e96e71cc83c7655e345e0526098
Hasnain Shaukat <hasnain.decojent@gmail.com>
2023-06-27 16:07:38 +0500

    few changes

--- slider.js
+++ slider.js
@@ -1,8 +1,32 @@
-$(document).ready(function(){
-    $('.790').slick({
-        arrows: false,
-        infinite: true,
-        slidesToShow:  2,
-        slidesToScroll: 2
-    });
+$('.s1321').slick({
+    dots: false,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
   });
