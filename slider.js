cf3f40303131bfe73e7fba59dadd10b8ebc72169
Hasnain Shaukat <hasnain.decojent@gmail.com>
2023-07-05 11:36:42 +0500

    responsive slider ingerated

--- slider.js
+++ slider.js
@@ -1,32 +1,45 @@
-$('.s1321').slick({
+$(function () {
+  $('.responsive-slider').slick({
+    nextArrow: '.yellow_arrow',
     dots: false,
     infinite: true,
     speed: 300,
     slidesToShow: 2,
-    slidesToScroll: 1,
+    slidesToScroll: 2,
     responsive: [
       {
         breakpoint: 1024,
         settings: {
-          slidesToShow: 3,
-          slidesToScroll: 3,
+          nextArrow: '.yellow_arrow',
+          dots: false,
           infinite: true,
-          dots: true
+          speed: 300,
+          slidesToShow: 2,
+          slidesToScroll: 2,
         }
       },
       {
         breakpoint: 600,
         settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2
+          nextArrow: '.yellow_arrow',
+          dots: false,
+          infinite: true,
+          speed: 300,
+          slidesToShow: 1,
+          slidesToScroll: 1
         }
       },
       {
         breakpoint: 480,
         settings: {
+          nextArrow: '.yellow_arrow',
+          dots: false,
+          infinite: true,
+          speed: 300,
           slidesToShow: 1,
           slidesToScroll: 1
         }
       }
     ]
   });
+});
